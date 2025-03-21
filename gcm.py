a485ef0efa4468ae03465f071c6d8527cf2f3b67
Ronald Volgers <rvolgers@users.noreply.github.com>
2025-03-21 09:29:28 +0100

    entirely unify the loops in poly_divmod

--- gcm.py
+++ gcm.py
@@ -344,17 +344,12 @@ def poly_divmod(f, g, lc_g_inv = None):
 
     qr = f[:]
 
-    for i in reversed(range(rdigits, qdigits + rdigits)):
+    for i in reversed(range(0, qdigits + rdigits)):
         tmp = qr[i]
         for j in range(max(0, rdigits - (i + 1)), min(rdigits, qdigits + rdigits - (i + 1))):
             tmp ^= gf_mul(qr[i + 1 + j], g[len(g) - 2 - j])
-        tmp = gf_mul(tmp, lc_g_inv)
-        qr[i] = tmp
-
-    for i in reversed(range(rdigits)):
-        tmp = qr[i]
-        for j in range(max(0, rdigits - (i + 1)), min(rdigits, qdigits + rdigits - (i + 1))):
-            tmp ^= gf_mul(qr[i + 1 + j], g[len(g) - 2 - j])
+        if i >= rdigits:
+            tmp = gf_mul(tmp, lc_g_inv)
         qr[i] = tmp
 
     r, q = qr[:rdigits], qr[rdigits:]
