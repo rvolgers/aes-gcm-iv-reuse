6e2de0b6811a809d3784eee5bdd30982b97f4a1b
Ronald Volgers <rvolgers@users.noreply.github.com>
2025-03-21 09:19:45 +0100

    unify the loop conditions in poly_divmod

--- gcm.py
+++ gcm.py
@@ -345,23 +345,23 @@ def poly_divmod(f, g, lc_g_inv = None):
     q = [0] * qdigits
     r = f[:]
 
-    for i in reversed(range(qdigits)):
+    for i in reversed(range(rdigits, qdigits + rdigits)):
         # previous q's, most recent first: prev_qs = q[i+1:]
         # lets index into this slice with j in range(len(prev_qs))
         # from the most recent, we want the second coefficient of g, which is at g[len(g)-2]
         # putting that in terms of j: g[len(g)-2-j]
         # this puts a second upper bound on j: j <= len(g) - 2 (or equivalently: j < len(g) - 1)
-        lc = r[i + rdigits]
-        for j in range(min(rdigits, qdigits - (i + 1))):
-            lc ^= gf_mul(q[i + 1 + j], g[len(g) - 2 - j])
+        lc = r[i]
+        for j in range(max(0, rdigits - (i + 1)), min(rdigits, qdigits + rdigits - (i + 1))):
+            lc ^= gf_mul(q[i + 1 + j - rdigits], g[len(g) - 2 - j])
         print(f"new: lc {i} = {lc}")
         if lc != 0:
             digit = gf_mul(lc, lc_g_inv)
-            q[i] = digit
-            r[i + rdigits] = 0
+            q[i - rdigits] = digit
+            r[i] = 0
 
     for i in reversed(range(rdigits)):
-        for j in range(rdigits - (i + 1), min(rdigits, qdigits + rdigits - (i + 1))):
+        for j in range(max(0, rdigits - (i + 1)), min(rdigits, qdigits + rdigits - (i + 1))):
             q_i = i + 1 + j - rdigits
             g_i = len(g) - 2 - j
             assert q_i >= 0
